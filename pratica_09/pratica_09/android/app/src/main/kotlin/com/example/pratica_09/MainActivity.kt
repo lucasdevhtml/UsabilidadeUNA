8dcbc886d77049d26bb848ad09c8b8014ab84274
Lucas_aguiar <lucas.9thadeu@gmail.com>
2020-10-01 21:17:10 -0300

    atividade 9

---
+++ pratica_09/pratica_09/android/app/src/main/kotlin/com/example/pratica_09/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pratica_09
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pratica_09/pratica_09/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  int numeroVezes = 0;
+  String mensagem = "Esse numero é: ";
+  void cliqueDoBotao() {
+    numeroVezes = numeroVezes + 1;
+    mensagem = "${numeroVezes % 2 == 0 ? "par" : "ímpar"}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Página Inicial"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "Número de vezes em que o botão foi pressionado: $numeroVezes \n$mensagem."),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(cliqueDoBotao);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
